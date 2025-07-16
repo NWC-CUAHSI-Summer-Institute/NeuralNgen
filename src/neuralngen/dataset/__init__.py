4bf48200e7b670a774c2657945586f6177a81cc4
jmframe <jonframe@gmail.com>
2025-07-16 10:19:07 -0500

    adding daily data

--- src/neuralngen/dataset/__init__.py
+++ src/neuralngen/dataset/__init__.py
@@ -3,11 +3,13 @@
 from neuralngen.dataset.basedataset import BaseDataset
 from neuralngen.dataset.camelsus import CamelsUS
 from neuralngen.dataset.hourlycamelsus import HourlyCamelsDataset
+from neuralngen.dataset.dailycamelsus import DailyCamelsDataset
 from neuralngen.dataset.collate import custom_collate
 
 __all__ = [
     "BaseDataset",
     "CamelsUS",
     "HourlyCamelsDataset",
+    "DailyCamelsDataset",
     "custom_collate"
 ]
